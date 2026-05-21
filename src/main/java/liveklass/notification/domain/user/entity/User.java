0297f47ec0dce9e45d47771c2b32178ba897fdd7
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2026-05-21 23:04:59 +0900

    feat: User 엔티티 생성

---
+++ src/main/java/liveklass/notification/domain/user/entity/User.java
@@ -0,0 +1,29 @@
+package liveklass.notification.domain.user.entity;
+
+import jakarta.persistence.*;
+import liveklass.notification.global.common.BaseEntity;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    
+
+
+}
