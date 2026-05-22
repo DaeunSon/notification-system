1daa13b071e6e807b02bc36ad4b03ff0b79911a6
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2026-05-22 14:08:37 +0900

    feat: NotificationRepository 생성

---
+++ src/main/java/liveklass/notification/domain/notification/repository/NotificationRepository.java
@@ -0,0 +1,24 @@
+package liveklass.notification.domain.notification.repository;
+
+import liveklass.notification.domain.notification.entity.Notification;
+import liveklass.notification.domain.notification.entity.NotificationChannel;
+import liveklass.notification.domain.notification.entity.NotificationType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    boolean existsByReceiver_IdAndNotificationTypeAndReferenceIdAndChannel(
+            Long receiverId,
+            NotificationType notificationType,
+            String referenceId,
+            NotificationChannel channel
+    );
+
+    List<Notification> findByReceiver_IdOrderByIdDesc(Long receiverId);
+
+    List<Notification> findByReceiver_IdAndReadOrderByIdDesc(Long receiverId, boolean read);
+}
