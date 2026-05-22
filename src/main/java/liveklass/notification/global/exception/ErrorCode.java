7329b7017e733b5fe418686d002c1508e40ba255
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2026-05-22 16:28:40 +0900

    chore: ErrorCode 에러 추가

--- src/main/java/liveklass/notification/global/exception/ErrorCode.java
+++ src/main/java/liveklass/notification/global/exception/ErrorCode.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
 
+    //Common
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, "요청 값이 올바르지 않습니다."),
+
     //User
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
 
